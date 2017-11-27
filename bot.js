a6833557b64639fa0542840f1ed93b8a3437fc1d
Kohishi <32710349+Kohishi@users.noreply.github.com>
2017-11-27 21:30:31 +0530

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.on('ready', () => {
+  console.log('I am ready, master!');
+  });
+client.on('message', message=> {
+if(message.content==='ping') {
+message.reply('pong');}
+});
+client.login(process.env.BOT_TOKEN);
